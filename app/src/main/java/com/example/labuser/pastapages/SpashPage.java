fa97a49994033110e01aaa8045ca2f0bca19d70e
LabUser <zachary.sylvester@maine.edu>
2016-09-20 13:35:37 -0400

    Initial commit

---
+++ app/src/main/java/com/example/labuser/pastapages/SpashPage.java
@@ -0,0 +1,13 @@
+package com.example.labuser.pastapages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpashPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash_page);
+    }
+}
